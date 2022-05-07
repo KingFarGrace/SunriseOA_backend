91055945e1c22b9ad70ee273b595808b8e998ccc
KingFarGrace <KingFarGrace.163.com>
2022-05-07 11:01:13 +0800

    First commit - initialized a springboot framework

---
+++ src/main/java/com/sunrise/SunriseOaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sunrise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SunriseOaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SunriseOaBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sunrise/SunriseOaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sunrise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SunriseOaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
